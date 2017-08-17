004b612a5f792021fd0849217121d20622fc574e
vardan <vardan.gaur007@gmail.com>
2017-08-18 03:28:21 +0530

    Add files via upload

---
+++ NOVATE.ino
@@ -0,0 +1,56 @@
+#include <LiquidCrystal.h>
+int count = 0;                                          
+char input[12];                                         
+boolean flag = 0; 
+const int pwm = 2 ; //initializing pin 2 as pwm
+//const int in_1 = 8 ;
+//const int in_2 = 9 ; // flag =0
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup()
+{
+   Serial.begin(9600); 
+   //pinMode(pwm,OUTPUT) ; //we have to set PWM pin as output
+  // pinMode(in_1,OUTPUT) ; //Logic pins are also set as output
+   //pinMode(in_2,OUTPUT) ;   // begin serial port with baud rate 9600bps
+  lcd.begin(16, 2);
+   
+}
+void loop()
+{
+   if(Serial.available())
+   {
+      count = 0;
+      while(Serial.available() && count < 12)          
+      {
+         input[count] = Serial.read();
+         count;
+         delay(100);
+      }
+      Serial.print(input);                             
+      delay(1000);
+      if((input[0] ^ input[2] ^ input[4] ^ input[6] ^ input[8] == input[10]) && 
+         (input[1] ^ input[3] ^ input[5] ^ input[7] ^ input[9] == input[11]))
+ 
+              Serial.println(" hello rajasthan ");
+            
+      else
+            Serial.println("Error");
+    Serial.print(input);                             // Print RFID tag number 
+      if((input[0] ^ input[2] ^ input[4] ^ input[6] ^ input[8] == input[10]) && 
+         (input[1] ^ input[3] ^ input[5] ^ input[7] ^ input[9] == input[11]))
+           lcd.setCursor(0,0);
+           lcd.print(" Rajasthan Hakathon");
+           
+           delay(2000);
+           lcd.setCursor(0,1);
+           lcd.print("It is awsm");
+           
+           
+            
+   }else
+                lcd.print("");           
+   }   
+
+
+     
---
+++ main_activity.java
@@ -0,0 +1,50 @@
+package com.example.simranjeetsingh.myapplication;
+
+
+import android.bluetooth.le.ScanResult;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private ZXingScannerView zXingScannerView;
+    ScanResult scanResult;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void scan(View view){
+        zXingScannerView =new ZXingScannerView(getApplicationContext());
+        setContentView(zXingScannerView);
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        zXingScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Toast.makeText(getApplicationContext(),result.getText(),Toast.LENGTH_SHORT).show();
+        zXingScannerView.resumeCameraPreview(this);
+
+    }
+}
